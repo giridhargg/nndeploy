f222886c36afebb5a190025234ba7736587c8c88
Giridhar Reddy Gangireddy <giridhar.reddy012@gmail.com>
2023-05-02 13:43:45 -0400

    Removed comments

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
-from flask import Flask, jsonify, request, url_for, redirect, render_template
-import pandas as pd
-# from flask_cors import CORS
-import pickle
+from flask import Flask, jsonify, request, render_template
 from model import get_model
 
 from pathlib import Path
 THIS_FOLDER = Path(__file__).parent.resolve()
-# my_file = THIS_FOLDER / "nn_model.pkl"
-# model = pickle.load(open(my_file, 'rb'))
 
 model = get_model()
 
 weights_file = THIS_FOLDER / "weights.h5"
-# weights = pickle.load(open(weights_file, 'rb'))
 
 model.load_weights(weights_file)
 
 app = Flask(__name__)
-# CORS(app)
+
 @app.route('/')
 def home():
     return render_template("ui.html")
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    input_data = request.json['input']
-    input_data = [[float(i) for i in row] for row in input_data]
+    input_data = [[float(i) for i in row] for row in request.json['input']]
     y_pred_val = model.predict(input_data)
     y_pred_val_labels = (y_pred_val >= 0.5).astype(int)
     for i in range(len(input_data)):
--- model.py
+++ model.py
@@ -1,28 +1,13 @@
-import pandas as pd
-import seaborn as sns
-import numpy as np
-import pickle
-import keras
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from keras import layers
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dropout
-from keras.optimizers import SGD
-from keras.losses import BinaryCrossentropy
-from sklearn.metrics import confusion_matrix
 
 
 def get_model():
     model = Sequential()
-    model.add(Dense(8,input_dim=7,activation="relu")) #input layer
-    model.add(Dense(5,activation='relu')) #hidden layer 1
+    model.add(Dense(8,input_dim=7,activation="relu"))
+    model.add(Dense(5,activation='relu'))
 
-    model.add(Dense(3,activation='relu')) #hidden layer 2
-    model.add(Dense(1,activation='sigmoid')) #output layer
+    model.add(Dense(3,activation='relu'))
+    model.add(Dense(1,activation='sigmoid'))
 
     return model
